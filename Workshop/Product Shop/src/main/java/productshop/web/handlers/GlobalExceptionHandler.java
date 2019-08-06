9afa1271995a47f76b955ebd0e8a993c2e4abdf4
Taewii <p3rfec7@abv.bg>
2019-08-06 21:37:13 +0300

    some error handling and error pages

--- Workshop/Product Shop/src/main/java/productshop/web/handlers/GlobalExceptionHandler.java
+++ Workshop/Product Shop/src/main/java/productshop/web/handlers/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
+import java.util.NoSuchElementException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -11,5 +13,10 @@ public class GlobalExceptionHandler {
     public String maxUploadSizeExceptionHandler(MaxUploadSizeExceededException e) {
         // TODO: 19.7.2019 г. needs to be handled better.
         return "redirect:/products/add";
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public String handleNoSuchElementException(NoSuchElementException e) {
+        return "error/no-such-element-error";
     }
 }
