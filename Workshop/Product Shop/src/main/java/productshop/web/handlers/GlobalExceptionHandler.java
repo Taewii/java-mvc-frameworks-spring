ee34dada8aad463dc8a96818ee5ca3f4d9b9d970
Taewii <p3rfec7@abv.bg>
2019-08-07 20:08:42 +0300

    some error handling and error pages

--- Workshop/Product Shop/src/main/java/productshop/web/handlers/GlobalExceptionHandler.java
+++ Workshop/Product Shop/src/main/java/productshop/web/handlers/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package productshop.web.handlers;
 
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.util.NoSuchElementException;
@@ -11,12 +13,26 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public String maxUploadSizeExceptionHandler(MaxUploadSizeExceededException e) {
-        // TODO: 19.7.2019 г. needs to be handled better.
         return "redirect:/products/add";
     }
 
     @ExceptionHandler(NoSuchElementException.class)
     public String handleNoSuchElementException(NoSuchElementException e) {
         return "error/no-such-element-error";
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public String handleForbiddenRequestException() {
+        return "error/403";
+    }
+
+    @ExceptionHandler(HttpClientErrorException.NotFound.class)
+    public String handleNotFoundRequestException() {
+        return "error/404";
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public String handleEveryUnhandledException() {
+        return "error/error";
     }
 }
